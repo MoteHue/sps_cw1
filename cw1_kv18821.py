f8277dd33802bed82e45fdd9febdbabac677fe9e
Matthew <kv18821@bristol.ac.uk>
2020-02-20 10:54:35 +0000

    You can't just shove it all into X/A.
    Functions now come in threes.

--- cw1_kv18821.py
+++ cw1_kv18821.py
@@ -5,11 +5,22 @@ import matplotlib.pyplot as plt
 from enum import Enum
 
 # Calculate the Least Squares Regression.
-def leastSquares(xs, ys):
+def leastSquaresLinear(xs, ys):
+    ones = np.ones(xs.shape)
+    X = np.column_stack((ones, xs))
+    A = np.linalg.inv(X.T @ X) @ X.T @ ys
+    return A
+def leastSquaresQuad(xs, ys):
+    ones = np.ones(xs.shape)
+    X = np.column_stack((ones, xs, xs**2))
+    A = np.linalg.inv(X.T @ X) @ X.T @ ys
+    return A
+def leastSquaresCubic(xs, ys):
     ones = np.ones(xs.shape)
     X = np.column_stack((ones, xs, xs**2, xs**3))
     A = np.linalg.inv(X.T @ X) @ X.T @ ys
     return A
+
 
 # Calculate the residual error with y-squared differences.
 def ySquared(xs, ys, A, lineType):
@@ -26,8 +37,8 @@ def ySquared(xs, ys, A, lineType):
     return np.sum(squared)
 
 # Calculate the residual error.
-def resError(xs, ys, A):
-     return [ySquared(xs, ys, A, "linear"), ySquared(xs, ys, A, "quad"), ySquared(xs, ys, A, "cubic")]#, ySquared(xs, ys, A, "exp")]
+def resError(xs, ys, AL, AQ, AC):
+     return [ySquared(xs, ys, AL, "linear"), ySquared(xs, ys, AQ, "quad"), ySquared(xs, ys, AC, "cubic")]#, ySquared(xs, ys, A, "exp")]
 
 # Load the points from the file specified in the command line.
 points = util.load_points_from_file(sys.argv[1])
@@ -45,14 +56,20 @@ for i in range(noOfChunks):
     ys.append(ypoints[i*20 : (i+1)*20])
 
 # Assign the least squares results to a matrix A.
-A = []
+ALinear = []
 for i in range(noOfChunks):
-    A.append(leastSquares(xs[i], ys[i]))
+    ALinear.append(leastSquaresLinear(xs[i], ys[i]))
+AQuad = []
+for i in range(noOfChunks):
+    AQuad.append(leastSquaresQuad(xs[i], ys[i]))
+ACubic = []
+for i in range(noOfChunks):
+    ACubic.append(leastSquaresCubic(xs[i], ys[i]))
 
 # Assign the residual errors into an array.
 resErrors = []
 for i in range(noOfChunks):
-    resErrors.append(resError(xs[i], ys[i], A[i]))
+    resErrors.append(resError(xs[i], ys[i], ALinear[i], AQuad[i], ACubic[i]))
 
 # Plot the line of best fit.
 # The colour represents the line type:
@@ -60,17 +77,17 @@ for i in range(noOfChunks):
 #   - quadratic     = green
 #   - cubic         = yellow
 #   - exponential   = red
-def plotBestFit(xs, ys, A, resErrors):
+def plotBestFit(xs, ys, AL, AQ, AC, resErrors):
     if min(resErrors) == resErrors[0]:
-        plt.plot(xs, np.poly1d([A[1],A[0]])(xs), color="b")
+        plt.plot(xs, np.poly1d([AL[1],AL[0]])(xs), color="b")
     elif min(resErrors) == resErrors[1]:
-        plt.plot(xs, np.poly1d([A[2],A[1],A[0]])(xs), color="g")
+        plt.plot(xs, np.poly1d([AQ[2],AQ[1],AQ[0]])(xs), color="g")
     elif min(resErrors) == resErrors[2]:
-        plt.plot(xs, np.poly1d([A[3],A[2],A[1],A[0]])(xs), color="y")
+        plt.plot(xs, np.poly1d([AC[3],AC[2],AC[1],AC[0]])(xs), color="y")
     #elif min(resErrors) == resErrors[4]:
     #    plt.plot(xs, A[0] + A[4]*np.exp(xs), color="r")
 
 # Plot the graph.
 for i in range(noOfChunks):
-    plotBestFit(xs[i], ys[i], A[i], resErrors[i])
+    plotBestFit(xs[i], ys[i], ALinear[i], AQuad[i], ACubic[i], resErrors[i])
 util.view_data_segments(xpoints, ypoints)
